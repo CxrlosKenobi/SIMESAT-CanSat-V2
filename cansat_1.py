665eff3d50b90982a1491b556f18ae025d08db8d
CxrlosKenobi <yolitopinto@gmail.com>
2020-12-01 20:59:07 -0300

    Update: Hardware recognition

---
+++ cansat_1.py
@@ -0,0 +1,41 @@
+'''
+Módulo GY-91
+- Acelerómetro
+- Giroscopio
+- Magnetómetro
+- Presión
+- Temperatura
+'''
+import pepitocompa
+# CodeExample
+
+
+'''
+GPS Ublox NEO-6M
+- Latitud, Longitud, Velocidad, Altitud
+'''
+
+'''
+GY-213 HDC1080
+- Humedad
+- Temperatura
+'''
+
+'''
+Placa STM32F103C8T
+    - Temporizador.
+    - Reloj integrado.
+    - Conversor de senñal analógica a digial(viceversa)
+'''
+
+
+'''
+Módulo LoRa RFM95W para 915MHz | Marca: HopeRF
+    - Receptor y emisor de paquetes.
+    -
+'''
+
+'''
+Buzzer
+    - Emite pitidos.
+'''
