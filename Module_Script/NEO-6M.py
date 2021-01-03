d3781593a2ed07e03e2ffd6337a06b735d159591
Rodrigo Flores <rodrigo.flores.12.07@gmail.com>
2021-01-03 02:17:48 -0300

    Update: Adding some code to NEO-6M

--- Module_Script/NEO-6M.py
+++ Module_Script/NEO-6M.py
@@ -29,3 +29,23 @@ serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
 while True:
     str = serialPort.readline()
     parseGPS(str)
+
+# Third Option
+port = "/dev/ttyAMA0"
+
+ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
+
+while 1:
+    try:
+        data = ser.readline()
+    except:
+        print("loading")
+
+    if data[0:6] == '$GPGGA':
+        msg = pynmea2.parse(data)
+        latval = msg.lat
+        concatlat = "lat:" + str(latval)
+        print(concatlat)
+        longval = msg.lon
+        concatlong = "long:" + str(longval)
+        print(concatlong, concatlat)
