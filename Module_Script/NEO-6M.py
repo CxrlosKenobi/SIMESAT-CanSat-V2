6a3800b52bc7eefc99d77ba4b387ca76d4cc4c62
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2021-01-03 00:09:12 -0300

    Update: Adding a few of things to NEO-6M

---
+++ Module_Script/NEO-6M.py
@@ -0,0 +1,31 @@
+import pynmea2
+import serial
+
+# First Option
+while True:
+    port = '/dev/ttyAMA0'
+    ser = serial.Serial(port, baudrate=9600, timeout=0.5)
+    dataout = pynmea2.NMEAStreamReader()
+    newdata = ser.readline()
+
+    if newdata[0:6] == "$GPRMC":
+        newmsg = pynmea2.parse(newdata)
+        lat = newmsg.latitude
+        long = newmsg.longitude
+        gps = "Lat: " + str(lat) + " Long: " + str(long
+
+        print(gps)
+
+#return gps
+
+# Second Option
+def parseGPS(str):
+    if str.find('GGA') > 0:
+        msg = pynmea2.parse(str)
+        print "Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units)
+
+serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
+
+while True:
+    str = serialPort.readline()
+    parseGPS(str)
