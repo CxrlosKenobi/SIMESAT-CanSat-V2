de058a4e87cb6e0e336e8c171492d5cb85209a51
Rodrigo-Flores <rodrigo.flores.12.06@gmail.com>
2021-01-13 12:04:26 -0300

    Update: Adding Markdown and finxing some bugs.

--- Module_Script/HDC-1080/HDC1080.py
+++ Module_Script/HDC-1080/HDC1080.py
@@ -1,43 +1,27 @@
 import sys
-sys.path.append('./SDL_Pi_HDC1080_Python3')
 from colorama import Fore, Back, Style
 import SDL_Pi_HDC1080
 from datetime import * 
 import time
 import csv
-
-print ("")
-print ("Reading Temperature and Humidity from HDC1080 Module ")
-print ("")
-
 import sys
 import os
 sys.path.append('./SDL_Pi_HDC1080_Python3')
 os.system('clear')
 
-print ("\nRead Temperature and Humidity from HDC1080 using I2C bus.\n")
-import sys
-import os
-sys.path.append('./SDL_Pi_HDC1080_Python3')
-os.system('clear')
-
-
-print ("\nRead Temperature and Humidity from HDC1080 using I2C bus.\n")
 hdc1080 = SDL_Pi_HDC1080.SDL_Pi_HDC1080()
 
 with open('HDC-1080-2.csv', 'w', newline='') as file:
 	write = csv.writer(file)
 	write.writerow(['Time', 'Temperature(C)', 'Humidity'])
 
-while True:
-	current_time = datetime.datetime.now()
-	
 HH = 0
 MM = 0
 SS = 0
+
 while True:
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
+	now = datetime.now()
+	current_time = now.strftime("%H:%M:%S")
 	if SS == 60:
 		MM += 1
 		SS = 0
@@ -46,27 +30,16 @@ while True:
 		HH += 1
 		MM = 0
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-	with open('HDC-1080-2.csv', 'a+', newline='') as file:
-=======
->>>>>>> 9736f085fddd37672d90009b1caa9922289c5418
->>>>>>> 22744c122abffb3a398fe45c7e34fc9b714e3349
 	with open('HDC-1080.csv', 'a+', newline='') as file:
->>>>>>> fbbdebdbe6046001078a5d4fd3c2100fb6b9dd08
 		writer = csv.writer(file)
 		writer.writerow([current_time, hdc1080.readTemperature(), hdc1080.readHumidity()])
 
-	print (Fore.RED + f"|-------{now.strftime('%H:%M:%S')}-------|")
-	print (Fore.WHITE + f'Temperature = {hdc1080.readTemperature()} C°')
-	print (Fore.WHITE + f'Humidity = {hdc1080.readHumidity()}')
-	print (Fore.RED +  "|----------------------|" + Style.RESET_ALL)
-	time.sleep(5)
+	temperature = round(hdc1080.readTemperature(),1)
+	humidity = round(hdc1080.readHumidity(),1)
 
 	print (Fore.RED + "|-------" + Fore.WHITE + f'{HH}:{MM}:{SS}' + Fore.RED + "-------|")
-	print (Fore.WHITE + f'Temperature = {hdc1080.readTemperature()} C°')
-	print (Fore.WHITE + f'Humidity = {hdc1080.readHumidity()}')
+	print (Fore.WHITE + f'Temperature = {temperature} C°')
+	print (Fore.WHITE + f'Humidity = {humidity} %')
 	print (Fore.RED + "|----------------------|\n\n" + Style.RESET_ALL)
 
 	SS += 5
--- Module_Script/NEO-6M/NEO-6M.py
+++ Module_Script/NEO-6M/NEO-6M.py
@@ -4,14 +4,12 @@ import pynmea2
 import serial
 import string
 import time
-<<<<<<< HEAD
+
 
 os.system("touch NEO-6M.csv")
 os.system("echo >> NEO-6M.csv time,latitude,longitude,altitude")
-=======
-from datetime import datetime
 
->>>>>>> 9736f085fddd37672d90009b1caa9922289c5418
+from datetime import datetime
 
 def parseGPS(str):
     now = datetime.now()
@@ -19,8 +17,6 @@ def parseGPS(str):
     if str.find('GGA') > 0:
         msg = pynmea2.parse(str)
         print "Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % (current_time,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units)
-<<<<<<< HEAD
-=======
     
         time = current_time
         lat = msg.lat,msg.lat_dir
@@ -29,8 +25,6 @@ def parseGPS(str):
         
         os.system("echo >> NEO-6M.csv " + "%s,%s %s,%s %s,%s %s" % (current_time,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units)
 )    
-
->>>>>>> fbbdebdbe6046001078a5d4fd3c2100fb6b9dd08
 
 serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
 
