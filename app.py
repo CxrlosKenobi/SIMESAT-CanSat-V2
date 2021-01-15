05c69168b42a9d3332f236e4a0472fab6eb1ee27
Rodrigo-Flores <rodrigo.flores.12.06@gmail.com>
2021-01-15 04:32:05 -0300

    App ready v0.1

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ import sys
 import os
 sys.path.append('./SDL_Pi_HDC1080_Python3')
 hdc1080 = SDL_Pi_HDC1080.SDL_Pi_HDC1080()
+X = deque(maxlen=30)
+X.append(1)
+Y = deque(maxlen=30)
+Y.append(1)
 
 mpu = MPU9250(
     address_ak=AK8963_ADDRESS,
@@ -94,6 +98,18 @@ app.layout = html.Div(
         							[html.H6("Live GPS",
         							className='graph__title')]
         						),
+                                                        dcc.Graph(
+                                                                id = 'live-graph',
+                                                                figure = dict(
+                                                                	layout = dict(
+                                                                        	plot_bgcolor = app_color['graph_bg'],
+                                                                                paper_bgcolor = app_color['graph_bg'],
+                                                                        ),
+                                                                ),
+                                                        ),
+                                                        dcc.Interval(
+                                                                id = 'graph-update'
+                                                        ),
         					],
         				),
         			],
@@ -214,7 +230,7 @@ def update_graph_scatter(n):
         plot_bgcolor = app_color['graph_bg'],
         paper_bgcolor = app_color['graph_bg'],
         font = {'color':'#fff'},
-        height = 250,
+        height = 500,
         xaxis = {
             'range':[-5,5],
             'showline':True,
@@ -259,6 +275,7 @@ def update_graph_scatter(n):
         paper_bgcolor = app_color['graph_bg'],
         font = {'color':'#fff'},
         height = 250,
+        autosize = False,
         xaxis = {
             'range':[-5,5],
             'showline':True,
@@ -293,19 +310,36 @@ def update_graph_scatter(input_data):
                 y = list(Y),
                 name = 'Scatter',
                 mode = 'lines+markers',
+                line = {'color':'#42C4F7'},
             )
     layout = go.Layout(
+                plot_bgcolor = app_color['graph_bg'],
+                paper_bgcolor = app_color['graph_bg'],
+                font = {'color':'#fff'},
+                height = 250,
+                autosize = True,
                 xaxis = dict(
                         range = [min(X), max(X)],
+                        showline = True,
+                        zeroline = False,
+                        fixedrange = True,
+                        tickvals = [0, 50, 100, 150, 200],
+                        ticktext = ['200', '150', '100', '50', '0'],
+                        title = 'Time elapsed (sec)'
                 ),
                 yaxis = dict(
-                            range = [min(Y)- .5, max(Y)+ .5]
-                        )
+                            range = [min(Y)- .5, max(Y)+ .5],
+                            showgrid = True,
+                            showline = True,
+                            fixedrange = True,
+                            zeroline = False,
+                            gridcolor = app_color['graph_line']
+                        ),
             )
 
     return {'data': [data], 'layout' : layout}
 
 
 if __name__ == '__main__':
-	app.run_server(debug=True)
+    app.run_server(host='192.168.10.37', debug=True)
 
