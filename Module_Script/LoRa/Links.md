f9c57affcc0060e2ce784f5b24f4be852fa6b2ca
CxrlosKenobi <yolitopinto@gmail.com>
2021-02-22 03:44:56 -0300

    Update: Adding some data abput LoRa RFM98x and other things

--- Module_Script/LoRa/Links.md
+++ Module_Script/LoRa/Links.md
@@ -1,2 +1,8 @@
 # Links to Review About RFM-98W/HOPERF (LoRa)
 - [Introducing LoRa](https://www.instructables.com/Introducing-LoRa-/)
+- [About HOPERF LoRa and Python Code](https://hackaday.io/project/11763-ais-repeater/log/42700-hoperf-rfm98-lora-raspberry-pi-python-code)
+- [LoRa tutorial from Hacker.io](https://www.hackster.io/ChrisSamuelson/lora-raspberry-pi-single-channel-gateway-cheap-d57d36)
+- [About pyLoraRFM9x](pypi.org/project/pyLoraRFMx/)
+
+# Media to View
+![Raspberry Pi Zero W Pinout](https://i2.wp.com/peppe8o.com/wp-content/uploads/2020/09/Raspberry-PI-Zero-Pinout-schema.jpg?resize=910%2C657&ssl=1)
--- Module_Script/NEO-6M/NEO-6M.py
+++ Module_Script/NEO-6M/NEO-6M.py
@@ -1,27 +1,70 @@
-import os
-import time
-import pynmea2
+#pyserial library is required for working
+from datetime import *
 import serial
-import string
+import csv
 import time
-from datetime import datetime
 
-def parseGPS(str):
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    if str.find('GGA') > 0:
-        msg = pynmea2.parse(str)
-        print "Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s" % (current_time,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units)
-    
-        time = current_time
-        lat = msg.lat,msg.lat_dir
-        lon = msg.lon,msg.lon_dir
-        alt =  msg.altitude,msg.altitude_units
-        
-        os.system("echo >> NEO-6M.csv " + "%s,%s %s,%s %s,%s %s" % (current_time,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units))    
+mport = '/dev/ttyAMA0'                     #choose your com port on which you connected your neo 6m GPS
+#mport = "/dev/ttyAMA0"            #for Raspberry Pi pins
+#mport = "/dev/ttyUSB0"            #for Raspberry Pi USB
 
-serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
+with open('NEO-6M.csv', 'w', newline='') as file:
+    write = csv.writer(file)
+    write.writerow(['Time', 'Latitude','Longitude'])
+
+def parseGPS(data):
+    if data[0:6] == "$GPGGA":
+        s = data.split(",")
+        if s[7] == '0' or s[7]=='00':
+            print ("no satellite data available")
+            return
+        time = s[1][0:2] + ":" + s[1][2:4] + ":" + s[1][4:6]
+        #print("-----------")
+        lat = decode(s[2])
+        lon = decode(s[4])
+        #alt = decode(s)
+        return lat,lon
+        #return lat,lon,alt
+
+def decode(coord):
+    l = list(coord)
+    for i in range(0,len(l)-1):
+            if l[i] == "." :
+                    break
+    base = l[0:i-2]
+    degi = l[i-2:i]
+    degd = l[i+1:]
+    #print(base,"   ",degi,"   ",degd)
+    baseint = int("".join(base))
+    degiint = int("".join(degi))
+    degdint = float("".join(degd))
+    degdint = degdint / (10**len(degd))
+    degs = degiint + degdint
+    full = float(baseint) + (degs/60)
+    #print(full)
+
+    return full
+
+ser = serial.Serial(mport,9600,timeout = 2)
 
 while True:
-    str = serialPort.readline()
-    parseGPS(str)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    try:
+        dat = ser.readline().decode()
+        mylat,mylon = parseGPS(dat)
+        #mylat,mylon,myalt = parseGPS(dat)
+        print("Lat: -", mylat, "---", "Lon: -", mylon)
+        #print("Lat: -", mylat, "---", "Lon: -", mylon, "---", "alt: ", myalt)
+
+        with open('NEO-6M.csv', 'a+', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([current_time, mylat, mylon])
+
+    except KeyboardInterrupt:
+        print('\nExiting...')
+        break
+
+    except:
+        continue
