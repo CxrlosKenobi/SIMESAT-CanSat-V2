3b693615dbece545455ac19fafdde400c1066d8b
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2021-02-24 13:06:30 -0300

    Update: Adding code to LoRa

---
+++ Module_Script/LoRa/LoRa Test/Pi3b-rx/test1-rx.py
@@ -0,0 +1,36 @@
+from raspi_lora import LoRa, ModemConfig # Importing libraries
+
+# This is our callback function that runs when a message is received
+"""
+def on_recv(payload):
+    print("From:", payload.header_from)
+    print("Received:", payload.message)
+    print("RSSI: {}; SNR: {}".format(payload.rssi, payload.snr))
+"""
+
+# Use chip select 0. GPIO pin 17 will be used for interrupts
+# The address of this device will be set to 2
+lora = LoRa(1, 17, 10, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True)
+lora.on_recv = on_recv
+
+lora.set_mode_rx() # Turn on the "recivier mode (rx)"
+
+# Testing to print recivied packages
+while True:
+    try:
+        print(lora.on_recv)
+    except:
+        print("Error")
+
+# Send a message to a recipient device with address 10
+# Retry sending the message twice if we don't get an  acknowledgment from the recipient
+"""
+message = "Hello there!"
+status = lora.send_to_wait(message, 10, retries=2)
+if status is True:
+    print("Message sent!")
+else:
+    print("No acknowledgment from recipient")
+"""
+# And remember to call this as your program exits...
+lora.close()
---
+++ Module_Script/LoRa/LoRa Test/Pi3b-rx/test2-rx.py
@@ -0,0 +1,20 @@
+from raspi_lora import LoRa, ModemConfig # Importing libraries
+
+# This is our callback function that runs when a message is received
+def on_recv(payload):
+    print("From:", payload.header_from)
+    print("Received:", payload.message)
+    print("RSSI: {}; SNR: {}".format(payload.rssi, payload.snr))
+
+
+# Use chip select 0. GPIO pin 17 will be used for interrupts
+# The address of this device will be set to 2
+sleep() # This is just a testing line.
+lora = LoRa(1, 17, 101, modem_config=ModemConfig.Bw125Cr45Sf2048, tx_power=14, acks=True)
+lora.on_recv = on_recv
+
+lora.set_mode_rx() # Turn on the "recivier mode (rx)"
+
+
+# And remember to call this as your program exits...
+lora.close()
