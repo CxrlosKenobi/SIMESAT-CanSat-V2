02cd312d697a23873729b57efb4ee36e74e9dcc6
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2021-02-28 14:00:40 -0300

    Update: Nothinf important

--- Module_Script/LoRa/LoRa Test/Pi3b-rx/test2-rx.py
+++ Module_Script/LoRa/LoRa Test/Pi3b-rx/test2-rx.py
@@ -8,9 +8,9 @@ def on_recv(payload):
 
 
 # Use chip select 0. GPIO pin 17 will be used for interrupts
-# The address of this device will be set to 2
+# The address of this device will be set to 101
 sleep() # This is just a testing line.
-lora = LoRa(1, 17, 101, modem_config=ModemConfig.Bw125Cr45Sf2048, tx_power=14, acks=True)
+lora = LoRa(1, 25, 101, modem_config=ModemConfig.Bw125Cr45Sf2048, tx_power=14, acks=False)
 lora.on_recv = on_recv
 
 lora.set_mode_rx() # Turn on the "recivier mode (rx)"
