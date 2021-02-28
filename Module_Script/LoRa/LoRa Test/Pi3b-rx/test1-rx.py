777987faf21c76845afabb589f33f40960056caa
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2021-02-28 13:58:32 -0300

    Update: Nothing important

--- Module_Script/LoRa/LoRa Test/Pi3b-rx/test1-rx.py
+++ Module_Script/LoRa/LoRa Test/Pi3b-rx/test1-rx.py
@@ -1,16 +1,14 @@
 from raspi_lora import LoRa, ModemConfig # Importing libraries
 
 # This is our callback function that runs when a message is received
-"""
 def on_recv(payload):
     print("From:", payload.header_from)
     print("Received:", payload.message)
     print("RSSI: {}; SNR: {}".format(payload.rssi, payload.snr))
-"""
 
 # Use chip select 0. GPIO pin 17 will be used for interrupts
 # The address of this device will be set to 2
-lora = LoRa(1, 17, 10, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True)
+lora = LoRa(1, 25, 10, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True)
 lora.on_recv = on_recv
 
 lora.set_mode_rx() # Turn on the "recivier mode (rx)"
--- Module_Script/LoRa/LoRa Test/Pi3b-rx/test2-rx.py
+++ Module_Script/LoRa/LoRa Test/Pi3b-rx/test2-rx.py
@@ -9,8 +9,8 @@ def on_recv(payload):
 
 # Use chip select 0. GPIO pin 17 will be used for interrupts
 # The address of this device will be set to 2
-sleep() # This is just a testing line.
-lora = LoRa(1, 17, 101, modem_config=ModemConfig.Bw125Cr45Sf2048, tx_power=14, acks=True)
+#sleep() # This is just a testing line.
+lora = LoRa(1, 25, 101, modem_config=ModemConfig.Bw125Cr45Sf2048, tx_power=14, acks=True)
 lora.on_recv = on_recv
 
 lora.set_mode_rx() # Turn on the "recivier mode (rx)"
