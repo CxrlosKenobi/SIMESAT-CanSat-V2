e030cb14fc88fb6cbbb049f1ec74805ef5381a1c
CxrlosKenobi <yolitopinto@gmail.com>
2021-05-28 03:23:52 -0400

    Update class BMP

--- modules/BMP280.py
+++ modules/BMP280.py
@@ -28,7 +28,7 @@ class BMP:
 
         # Calibration with 100 sampling
         for i in range(baseline_size):
-            baseline_values.append(pressure)
+            baseline_values.append(press())
 
         baseline = sum(baseline_values[:-25]) / len(baseline_values[:-25])
         altitude = self.bmp280.get_altitude(qnh=baseline)
