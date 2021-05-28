8e68c12fd27d8271a9edd929ae40c6f88803f9b5
CxrlosKenobi <yolitopinto@gmail.com>
2021-05-28 02:53:39 -0400

    Add feature to HDC class

--- modules/HDC1080.py
+++ modules/HDC1080.py
@@ -1,16 +1,16 @@
+from modules import SDL_Pi_HDC1080
 import sys
-import SDL_Pi_HDC1080
 import os
 
 class HDC:
 	def __init__(self):
-		sys.path.append('./SDL_Pi_HDC1080_Python3')
+		sys.path.append('./modules/SDL_Pi_HDC1080_Python3')
 		self.hdc1080 = SDL_Pi_HDC1080.SDL_Pi_HDC1080()
 
-	def temp(self):
-		temperature = round(self.hdc1080.readTemperature(), 3)
+	def temp(self, decimal):
+		temperature = round(self.hdc1080.readTemperature(), self.decimal)
 		return temperature
 
-	def hum(self):
-		humidity = round(self.hdc1080.readHumidity(), 3)
+	def hum(self, decimal):
+		humidity = round(self.hdc1080.readHumidity(), self.decimal)
 		return humidity
