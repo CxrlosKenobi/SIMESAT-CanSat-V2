38e1c3e92c2beb3d7882a89a6ae558c3165dc770
CxrlosKenobi <yolitopinto@gmail.com>
2021-05-28 04:22:01 -0400

    Update main struct and functions

--- main.py
+++ main.py
@@ -11,20 +11,28 @@ MPU = MPU() # MPU-9250
 HDC = HDC() # HDC-1080
 BMP = BMP() # BMP-280
 
-while True:
-    try:
-        NEO_lo = "NO DATA"
-        NEO_la = "NO DATA"
+def get_current_time():
+    now = dt.datetime.now()
+    total_time = (now.hour * 3600) + (now.minute * 60) + (now.second)
 
-        now = datetime.now() # Current time
-        payload = f"{now.strftime('%d/%m, %H:%M:%S')};{get_current_time()};{BMP.press(4)},{BMP.alt(4)};{HDC.temp()},{HDC.hum()};{NEO_la},{NEO_lo};{MPU.accel()};{MPU.gyros()};{MPU.magnet()}"
+    return total_time
 
-        transmitPackets(payload)
+def main():
+    NEO_lo = "NO DATA"
+    NEO_la = "NO DATA"
 
-        print("[ ok ] Payload sent...")
+    payload = f"{now.strftime('%d/%m, %H:%M:%S')};{get_current_time()};{BMP.press(4)},{BMP.alt(4)};{HDC.temp()},{HDC.hum()};{NEO_la},{NEO_lo};{MPU.accel()};{MPU.gyros()};{MPU.magnet()}"
 
-        sleep(1)
+    transmitPackets(payload)
+    print("[ OK ] Payload sent...")
 
-    except KeyboardInterrupt:
-        print("[ ! ] Exiting\n")
-        exit()
+    sleep(1)
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            main()
+        except KeyboardInterrupt:
+            print("[ ! ] Exiting\n")
+            exit()
